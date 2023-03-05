207a12d395d7e38ebb912030ec2efcddd7b6475e
Adamou <adamou0120@outlook.fr>
2023-03-05 07:58:59 +0100

    feat: add new customer

---
+++ fact_app/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'address',
+                    'genre', 'age', 'city', 'zip_code')
+
+
+class AdminInvoice(admin.ModelAdmin):
+    list_display = ('customer', 'save_by', 'invoice_date_time',
+                    'total', 'last_updated_date', 'paid', 'comment')
+
+
+class AdminArticle(admin.ModelAdmin):
+    list_display = ('invoice', 'name', 'quantity', 'unit_price', 'total')
+
+
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Invoice, AdminInvoice)
+admin.site.register(Article, AdminArticle)
---
+++ fact_app/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Customer(models.Model):
+    # Customer models definition
+
+    GENDER_TYPE = (
+        ("MASCULIN", "M"),
+        ("FEMININ", "F"),
+    )
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=64)
+    genre = models.CharField(max_length=100, choices=GENDER_TYPE)
+    age = models.CharField(max_length=12)
+    city = models.CharField(max_length=32)
+    zip_code = models.CharField(max_length=16)
+    created_date = models.DateField(auto_now_add=True)
+    save_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+
+    def __str__(self):
+        return self.name
+
+
+class Invoice(models.Model):
+
+    """
+    Name=Invoice (facture) model definition
+    Description:
+    author: adamou@gmail.com
+
+    """
+
+    INVOICE_TYPE = (
+        ("R", "RECU"),
+        ("P", "PROFORMA FACTURE"),
+        ("F", "FACTURE"),
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    invoice_date_time = models.DateTimeField(auto_created=True)
+    total = models.DecimalField(decimal_places=2, default=0.0, max_digits=12)
+    paid = models.BooleanField(default=False)
+    last_updated_date = models.DateField(null=True, blank=True)
+    invoice_type = models.CharField(max_length=1, choices=INVOICE_TYPE)
+    comment = models.TextField(null=True, max_length=100, blank=True)
+    save_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "Invoice"
+        verbose_name_plural = "Invoices"
+
+    def __str__(self) -> str:
+        return f"{self.customer.name}_{self.invoice_date_time}"
+
+    @property
+    def get_total(self):
+        articles = self.article_set.all()
+        total = sum(article.get_total for article in articles)
+
+
+class Article(models.Model):
+
+    """
+    Name=Article model definition
+    Description:
+    author: adamou@gmail.com
+
+    """
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(
+        decimal_places=2, default=0.0, max_digits=10)
+    total = models.DecimalField(decimal_places=2, default=0.0, max_digits=10)
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    @property
+    def get_total(self):
+        total = self.quantity * self.unit_price
